2f7680ec67981cae5ec041a368a3038072387f47
whyquas <qusaratonkachi@gmail.com>
2025-03-29 08:50:06 +0500

    Updated again problem 2

--- src/FindAverage.java
+++ src/FindAverage.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 class AverageFinder {
     public static void main(String[] args) {
-        int[] arr = {2, 4, 6, 8};
-        System.out.println("Average: " + findAverage(arr, arr.length));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) arr[i] = scanner.nextInt();
+        System.out.println("Average: " + findAverage(arr, n));
     }
     public static double findAverage(int[] arr, int n) {
         if (n == 1) return arr[0];
