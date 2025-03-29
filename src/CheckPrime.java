478f9cecb8a306bc9b0fe8f2b88466ef416359d5
whyquas <qusaratonkachi@gmail.com>
2025-03-29 08:51:03 +0500

    Updated again problem 3

--- src/CheckPrime.java
+++ src/CheckPrime.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 class PrimeChecker {
     public static void main(String[] args) {
-        int n = 29;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
         System.out.println(n + " is prime: " + isPrime(n, n - 1));
     }
     public static boolean isPrime(int n, int divisor) {
